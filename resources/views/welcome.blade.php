6361dd68104aa83a39716ab97652aab683054af4
Agent-003 <abbasova003@gmail.com>
2019-12-06 17:49:49 +0200

    modified:   resources/views/welcome.blade.php    modified:   routes/web.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,18 +80,16 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <div class="content">
+                    <div class="title m-b-md">
+                        Homework 10
+                    </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div class="links">
+                        <a href="{{ url('/about') }}">About</a>
+                        <a href="{{ url('/contact') }}">Contact</a>
+                        <a href="{{ url('/profile') }}">Profile</a>
+                    </div>
                 </div>
             </div>
         </div>
